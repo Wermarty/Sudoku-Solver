3028161db257e498a7ee87c26af3dd6e283d13fd
hunryandev <108347965+hunryandev@users.noreply.github.com>
2023-04-24 10:06:25 +0200

    Add files via upload
    final final

--- integration/read_sudoku.py
+++ integration/read_sudoku.py
@@ -1,11 +1,12 @@
 import sys
 import pathlib
-from matplotlib import pyplot as plt 
+import time
 
 from code_number import import_data_model,find_orientation,number_recognition,write_sudoku,plot_num_model
 from code_grid import final
 from code_grid import utils
 from code_grid import detect_grid
+from code_grid import get_cases
 
 
 def find_number(cases, tab_num_model, orientation_initial, name_sudoku_text, path_to_write):
@@ -18,11 +19,17 @@ def find_number(cases, tab_num_model, orientation_initial, name_sudoku_text, pat
 
 
 if __name__ == "__main__":
+    
+    t = time.time()
+    
     parser = utils. get_base_parser("read_sudoku")
     parser.add_argument("--size-case", help="size of the resized images of each case. 20 by default ", type=int)
-    parser.add_argument("--name",help=" name of the output file ", type=str)
     args = parser.parse_args()
 
+    input = "grille sudoku/sudoku.jpg"
+    if(args.input is not None):
+        input = args.input
+        
     img = utils.read(args.input)
     if(img is None):
         print("can't read images")
@@ -33,40 +40,25 @@ if __name__ == "__main__":
     if(args.size_case is not None):
         pixel_resize = args.size_case
 
-    [cases,theta] = final.read_grid(img,pixel_resize)
+    [cases, theta] = final.read_grid(img, pixel_resize)   
     
-    img = cases[0,3]
-    plt.figure()
-    plt.imshow(img)
-    plt.savefig("img")
+    print(f" cases sucessufully isolated, in : {round(time.time() - t,2)} s")
     
-    img = cases[1,1]
-    plt.figure()
-    plt.imshow(img)
-    plt.savefig("img2")
+    output_path = pathlib.Path().absolute() 
+    name = "sudoku_toresolve"
     
-    if(args.output == None):
-        output_path = pathlib.Path().absolute() 
-    else:
-        directory_path = pathlib.Path(args.output)
-        
-        if not directory_path.exists():
-            directory_path.mkdir(parents=True)
-        output_path = args.output
-        print(output_path)
+    if args.output is not None:
+        out = pathlib.Path(args.output)
+        output_path = out.parent
+        output_path.mkdir(parents=True, exist_ok=True)
+        name = out.stem
             
-        
      
     tab_num_model = import_data_model(pixel_resize)
-    if(args.name is None):
-        name = "toresolve"   
-    else :
-        name = args.name
-    
-    #plot_num_model(tab_num_model,1)
-    #detect_grid.print_grid_img(cases)
     
     orientation = find_number(cases,tab_num_model, theta, name,output_path)
-    
-    
+    print(f" sudoku sucessfully read , in : {round(time.time() - t,2)} s")
+
+    print(f"save in {output_path} in the file {name}.txt")
+
     
